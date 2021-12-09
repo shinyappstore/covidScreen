48a02e815917ecf4685ac396b5898129d85a80f8
Jesse Smith <jesse.smith@stjude.org>
2021-12-09 17:57:29 -0600

    Create prototype for profiling page

---
+++ R/mod_profiling.R
@@ -0,0 +1,29 @@
+#' profiling UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_profiling_ui <- function(id){
+  ns <- NS(id)
+  tags$div(
+    style = "max-width: 1280px; margin: auto",
+    plotOutput(ns("plot")),
+    mod_profiling_input_ui(ns("input"))
+  )
+}
+
+#' profiling Server Functions
+#'
+#' @noRd
+mod_profiling_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+    output$plot <- renderPlot(shinipsum::random_ggplot())
+
+
+  })
+}
