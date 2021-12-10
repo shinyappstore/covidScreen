b4efedc6e9f0f8fcb076d5e003ca42a7e049495c
Jesse Smith <jesse.smith@stjude.org>
2021-12-10 15:33:39 -0600

    Add rest of input code to profiling_input

--- R/mod_profiling_input.R
+++ R/mod_profiling_input.R
@@ -136,6 +136,92 @@ mod_profiling_input_server <- function(id){
     toggle_panel_server("vac_eff_link", input, time = 0.2)
     toggle_panel_server("test_link", input, time = 0.4)
     toggle_panel_server("symp_link", input, time = 0.6)
+
+    # Organizational inputs info
+    observeEvent(input$org_link_info, ct_info_server(p(HTML(
+      "Organizational inputs directly involve the organization. They are",
+      " <b>interventions</b> that can be used to keep COVID-19 under control",
+      " in the organization."
+    ))))
+
+    # Community inputs info
+    observeEvent(input$comm_link_info, ct_info_server(p(
+      "Community inputs define the context in which the organization resides.",
+      " They help determine the baseline infection risk of an organization."
+    )))
+
+    # Advanced inputs info
+    observeEvent(input$advanced_link_info, ct_info_server(p(HTML(
+      "Advanced inputs are related to properties of COVID-19, its tests, and",
+      " it vaccinations. They are 'advanced' in the sense that the average",
+      " user is likely not an expert on COVID-19, and may not know reasonable",
+      " values for these inputs. The defaults should suffice for many",
+      " settings, but users are encouraged to change the defaults if they do",
+      " not match your setting. The <b>Inputs</b> tab allows users to explore",
+      " the effects of these parameters (and others) in detail."
+    ))))
+
+    # Input validation
+    iv <- shinyvalidate::InputValidator$new()
+    iv$add_rule("n_org", shinyvalidate::sv_required())
+    iv$add_rule("n_org", shinyvalidate::sv_gte(0))
+    iv$add_rule("test_f_asymp_vac", shinyvalidate::sv_required())
+    iv$add_rule("test_f_asymp_vac", shinyvalidate::sv_gte(0))
+    iv$add_rule("test_f_asymp_vac", sv_not_lt_1())
+    iv$add_rule("test_f_asymp_unvac", shinyvalidate::sv_required())
+    iv$add_rule("test_f_asymp_unvac", shinyvalidate::sv_gte(0))
+    iv$add_rule("test_f_asymp_unvac", sv_not_lt_1())
+    iv$add_rule("inf_r_incid", shinyvalidate::sv_required())
+    iv$add_rule("inf_r_incid", shinyvalidate::sv_between(0, 1e5,
+                                                         message_fmt = "Must be between 0 and 100,000"))
+    iv$add_rule("inf_t_symp", shinyvalidate::sv_required())
+    iv$add_rule("inf_t_symp", shinyvalidate::sv_gte(0))
+    iv$add_rule("inf_t_presymp", shinyvalidate::sv_required())
+    iv$add_rule("inf_t_presymp", shinyvalidate::sv_gte(0))
+    iv$enable()
+
+    n_org <- reactive(req_gte(input$n_org, 0))
+    test_f_asymp_vac <- reactive(req_gte(input$test_f_asymp_vac, 0))
+    test_f_asymp_unvac <- reactive(req_gte(input$test_f_asymp_unvac, 0))
+    inf_r_incid <- reactive(req_between(input$inf_r_incid, 0, 1e5))
+    inf_t_symp <- reactive(req_gte(input$inf_t_symp, 0))
+    inf_t_presymp <- reactive(req_gte(input$inf_t_presymp, 0))
+
+    dist_args <- reactiveValues()
+
+    dist_args$n <- reactive(n_org())
+
+    dist_args$vac <- reactive(list(
+      p_comm = input$vac_p_comm * 1e-2,
+      p_org  = input$vac_p_org  * 1e-2,
+      eff    = input$vac_eff    * 1e-2
+    ))
+
+    dist_args$inf <- reactive(list(
+      p_incid   = inf_r_incid() / 1e5,
+      t_symp    = inf_t_symp(),
+      t_presymp = inf_t_presymp()
+    ))
+
+    dist_args$symp <- reactive(list(
+      p_inf_vac   = input$symp_p_inf_vac   * 1e-2,
+      p_inf_unvac = input$symp_p_inf_unvac * 1e-2,
+      p_uninf     = input$symp_p_uninf     * 1e-2
+    ))
+
+
+    dist_args$test <- reactive(list(
+      p_symp        = input$test_p_symp * 1e-2,
+      p_asymp_vac   = correct_freq(1 / test_f_asymp_vac()),
+      p_asymp_unvac = correct_freq(1 / test_f_asymp_unvac())
+    ))
+
+    dist_args$detect <- reactive(list(
+      sens = input$detect_sens * 1e-2,
+      spec = input$detect_spec * 1e-2
+    ))
+
+    dist_args
   })
 }
 
