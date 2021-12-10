990403e1192c2fb771b65e8dc1aa090793367a7f
Jesse Smith <jesse.smith@stjude.org>
2021-12-10 12:46:20 -0600

    Add inputs

--- R/mod_profiling_input.R
+++ R/mod_profiling_input.R
@@ -10,16 +10,115 @@
 mod_profiling_input_ui <- function(id){
   ns <- NS(id)
   tagList(
+    # Make sliders nicer-looking
+    shinyWidgets::chooseSliderSkin("Flat", color = "#29434e"),
     fluidRow(span(style = "margin-left: auto", select_input(ns("x_var"))),
              span(style = "width: 3rem"),
              span(style = "margin-right: auto", select_output(ns("y_var")))
     ),
-    # Organization
-    column(width = 4),
-    # Community
-    column(width = 4),
-    # Advanced
-    column(width = 4)
+    fluidRow(
+      # Organization
+      column(width = 4, toggle_panel(ns("org_link"),
+                                     init_visible = TRUE,
+                                     label = "Organization",
+                                     icon = icon("user-shield"),
+                                     tag_fn = h5,
+        # People in organization
+        num_input(ns("n_org"), "Organization Size (People)", 1e3),
+        # Unvaccinated testing frequency
+        num_input(ns("test_f_asymp_unvac"), "Unvaccinated Testing (Days)", 7),
+        # Vaccinated testing frequency
+        num_input(ns("test_f_asymp_vac"), "Vaccinated Testing (Days)", 0),
+        # % Vaccinated in organization
+        slider_pct(ns("vac_p_org"), "% Vaccinated (in Organization)")
+      )),
+      # Community
+      column(width = 4, toggle_panel(ns("comm_link"),
+                                     init_visible = TRUE,
+                                     label = "Community",
+                                     icon = icon("users"),
+                                     tag_fn = h5,
+        # Case rate per 100k per day in community
+        num_input(ns("inf_r_incid"), "Case Rate (per 100k per Day)", 250),
+        # % Vaccinated in community
+        slider_pct(ns("vac_p_comm"), "% Vaccinated (in Community)")
+      )),
+      # Advanced
+      column(width = 4, toggle_panel(ns("advanced_link"),
+                                     init_visible = TRUE,
+                                     label = "Advanced",
+                                     icon = icon("cog"),
+                                     tag_fn = h5,
+        # Vaccine efficacy
+        toggle_panel(ns("vac_eff_link"),
+                    init_visible = FALSE,
+                    label = "Vaccine Efficacy",
+                    icon = icon("syringe"),
+                    tag_fn = h6,
+          slider_pct(ns("vac_eff"), label = NULL, value = 70),
+          tags$br()
+        ),
+        # Test-related inputs
+        toggle_panel(ns("test_link"),
+                    init_visible = FALSE,
+                    label = "Testing",
+                    icon = icon("vial"),
+                    tag_fn = h6,
+                    tags$br(),
+          # Test sensitivity
+          slider_pct(ns("detect_sens"),
+                     label = "Test Sensitivity (%)",
+                     value = 85
+          ),
+          # Test specificity
+          slider_pct(ns("detect_spec"),
+                     label = "Test Specificity (%)",
+                     value = 99.5,
+                     min   = 90,
+                     step  = 0.1
+          ),
+          # % of symptomatic people that are tested
+          slider_pct(ns("test_p_symp"),
+                     label = "% Symptomatics Tested",
+                     value = 95
+          )
+        ),
+        # Symptom-related inputs
+        toggle_panel(ns("symp_link"),
+                    init_visible = FALSE,
+                    label  = "Symptoms",
+                    icon   = icon("head-side-cough"),
+                    tag_fn = h6,
+                    tags$br(),
+          # Symptomatic period
+          num_input(ns("inf_t_symp"),
+                    label = "Symptomatic Period (Days)",
+                    value = 10
+          ),
+          # Pre-symptomatic period
+          num_input(ns("inf_t_presymp"),
+                    label = "Pre-Symptomatic Period (Days)",
+                    value = 3
+          ),
+          # % of unvaccinated cases that are symptomatic
+          slider_pct(ns("symp_p_inf_unvac"),
+                     label = "% Symptomatic: Unvaccinated Cases"
+          ),
+          # % of vaccinated cases that are symptomatic
+          slider_pct(ns("symp_p_inf_vac"),
+                     label = "% Symptomatic: Vaccinated Cases",
+                     value = 30
+          ),
+          # % of non-cases that are symptomatic
+          slider_pct(ns("symp_p_uninf"),
+                     label = "% Symptomatic: Non-Cases",
+                     value = 2,
+                     max   = 10,
+                     step  = 0.1
+          )
+        )
+      ))
+    )
   )
 }
 
@@ -29,6 +128,14 @@ mod_profiling_input_ui <- function(id){
 mod_profiling_input_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    # Show/hide input panels
+    toggle_panel_server("org_link", input, time = 0.5)
+    toggle_panel_server("comm_link", input, time = 0.3)
+    toggle_panel_server("advanced_link", input, time = 0.4)
+    toggle_panel_server("vac_eff_link", input, time = 0.2)
+    toggle_panel_server("test_link", input, time = 0.4)
+    toggle_panel_server("symp_link", input, time = 0.6)
   })
 }
 
