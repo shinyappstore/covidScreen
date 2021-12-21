d11d222eeb3079552480fa862ff73b4f16fce3ae
Jesse <jesse.smith@stjude.org>
2021-12-21 17:13:41 -0600

    Implement organization column

--- R/mod_profiling_input2.R
+++ R/mod_profiling_input2.R
@@ -15,12 +15,8 @@ mod_profiling_input2_ui <- function(id){
              span(style = "margin-right: auto", select_output(ns("y_var")))
     ),
     fluidRow(
-      numericInput2(ns("n_org"),
-                    label = "Organization Size (People)",
-                    condition = "input.x_var == 'n_org'",
-                    ns = ns,
-                    value = 1e3,
-                    value2 = c(1e2, 1e3))
+      # Organization inputs
+      org_profiling_column(ns("org_link"), ns = ns)
     )
   )
 }
@@ -32,67 +28,161 @@ mod_profiling_input2_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-    # iv <- shinyvalidate::InputValidator$new()
-    # iv$add_rule("n_org", shinyvalidate::sv_required())
-    # iv$add_rule("n_org", sv_gte_multi(0))
+    # Validate inputs
+    iv <- shinyvalidate::InputValidator$new()
+    iv$add_rule("n_org-point", shinyvalidate::sv_required())
+    iv$add_rule("n_org-range-1", shinyvalidate::sv_required())
+    iv$add_rule("n_org-range-2", shinyvalidate::sv_required())
+    iv$enable()
 
+    # Input panel show/hide and info
+    org_panel_server()
 
+    # Choose range or point based on `x_var`
 
-    n_org <- reactive(if (input$x_var == "n_org") input$n_org2 else input$n_org)
+    # Organization
+    n_org              <- reactive_range("n_org")
+    test_f_asymp_unvac <- reactive_range("test_f_asymp_unvac")
+    test_f_asymp_vac   <- reactive_range("test_f_asymp_vac")
+    vac_p_org          <- reactive_range("vac_p_org")
 
-    list(n = reactiveValues(org = n_org))
+    # Organization size
+    n <- reactiveValues(org = n_org)
+
+    # Distribution arguments
+    dist_args <- reactiveValues()
+    dist_args$vac <- reactive(list(
+      p_org = vac_p_org() * 1e-2
+    ), label = "dist_args$vac()")
+    # dist_args$inf <- reactive(list())
+    # dist_args$symp <- reactive(list())
+    dist_args$test <- reactive(list(
+      f_asymp_unvac = test_f_asymp_unvac(),
+      f_asymp_vac   = test_f_asymp_vac()
+    ), label = "dist_args$test()")
+    # dist_args$detect <- reactive(list())
+
+    # Plotting variables
+    plt_vars <- reactiveValues(
+      x = reactive(input$x_var),
+      y = reactive(input$y_var)
+    )
+
+    # Return named list of reactiveValues objects
+    list(n = n, dist_args = dist_args, plt_vars = plt_vars)
   })
 }
 
-select_input <- function(id) {
-  selectInput(
-    id,
-    label = "Input (X-axis)",
-    width = "21rem",
-    choices = list(
-      Organization = list(
-        `Organization Size (People)` = "n_org",
-        `Unvaccinated Testing (Days)` = "test_f_asymp_unvac",
-        `Vaccinated Testing (Days)` = "test_f_asymp_vac",
-        `% Vaccinated (in Organization)` = "vac_p_org"
-      ),
-      Community = list(
-        `Case Rate (per 100k per day)` = "inf_r_incid",
-        `% Vaccinated (in Community)` = "vac_p_comm"
-      ),
-      Vaccination = list(`Vaccine Efficacy (%)` = "vac_eff"),
-      Testing = list(
-        `Test Sensitivity (%)` = "detect_sens",
-        `Test Specificity (%)` = "detect_spec",
-        `% Symptomatics Tested` = "test_p_symp"
-      ),
-      Symptoms = list(
-        `Symptomatic Period (Days)` = "inf_t_symp",
-        `Pre-Symptomatic Period (Days)` = "inf_t_presymp",
-        `% Symptomatic: Unvaccinated Cases` = "symp_p_inf_unvac",
-        `% Symptomatic: Vaccinated Cases` = "symp_p_inf_vac",
-        `% Symptomatic: Non-Cases` = "symp_p_uninf"
-      )
-    )
-  )
-}
+
+# Helpers (wrappers) -----------------------------------------------------------
 
 
-select_output <- function(id) {
-  shinyWidgets::radioGroupButtons(
-    inputId = id,
-    label = "Output (Y-Axis)",
-    choices = list(
-      Risk = "risk",
-      Benefit = "benefit",
-      Vaccination = "vac"
+#' Wrap `conditonalNumericRangeInput()` for Ease of Use
+#'
+#' Wraps the `conditonalNumericRangeInput()` defined elsewhere in covidtest for
+#' brevity.
+#'
+#' @inheritParam conditonalNumericRangeInput
+#' @param br Add a line break below the input element
+#'
+#' @inherit return conditonalNumericRangeInput
+num_input2 <- function(
+  id,
+  label,
+  value,
+  value2,
+  min = 0,
+  max = NA,
+  step = 1,
+  trigger = "x_var",
+  br = TRUE
+) {
+  id_quo <- rlang::enquo(id)
+  ns <- if (rlang::quo_is_call(id_quo)) rlang::call_fn(id_quo) else NS(NULL)
+  tagList(
+    conditionalNumericRangeInput(
+      id = id,
+      label = label,
+      value = value,
+      value2 = value2,
+      min = min,
+      max = max,
+      step = step,
+      trigger = trigger,
+      ns = ns
     ),
-    status = "info"
+    if (br) tags$br()
   )
 }
 
-## To be copied in the UI
-# mod_profiling_input2_ui("profiling_input2_ui_1")
 
-## To be copied in the server
-# mod_profiling_input2_server("profiling_input2_ui_1")
+#' Wrap `conditionalSliderRangeInput()` for Ease of Use
+#'
+#' Wraps the `conditonalSliderRangeInput()` defined elsewhere in covidtest for
+#' brevity.
+#'
+#' @inheritParam conditonalSliderRangeInput
+#' @param br Add a line break below the input element
+#'
+#' @inherit return conditonalSliderRangeInput
+slider_pct2 <- function(
+  id,
+  label,
+  value = 50,
+  value2 = c(0, 100),
+  min = 0,
+  max = 100,
+  step = 1,
+  trigger = "x_var",
+  prefix = NULL,
+  br = TRUE
+) {
+  id_quo <- rlang::enquo(id)
+  ns <- if (rlang::quo_is_call(id_quo)) rlang::call_fn(id_quo) else NS(NULL)
+  tagList(
+    conditionalSliderRangeInput(
+      id = id,
+      label = label,
+      value = value,
+      value2 = value2,
+      trigger = trigger,
+      ns = ns,
+      min = min,
+      max = max,
+      step = step,
+      prefix = prefix,
+      suffix = "%"
+    ),
+    if (br) tags$br()
+  )
+}
+
+
+#' Wrap `reactivePointRange()` for Consistent Nomenclature in Main App
+#'
+#' @inheritParams reactivePointRange
+#'
+#' @inherit return reactivePointRange
+reactive_range <- function(id) {
+  reactivePointRange(id)
+}
+
+# Helpers (shinyvalidate) ------------------------------------------------------
+
+
+sv_gte_multi <- function(
+  rhs,
+  message_fmt = "Must be greater than or equal to {rhs}.",
+  allow_na = FALSE,
+  allow_nan = FALSE,
+  allow_inf = FALSE
+) {
+  shinyvalidate::sv_gte(
+    rhs = rhs,
+    message_fmt = message_fmt,
+    allow_multiple = TRUE,
+    allow_na = allow_na,
+    allow_nan = allow_nan,
+    allow_inf = allow_inf
+  )
+}
